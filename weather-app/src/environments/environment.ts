0905cfc4c0c3f60cb3645b1fe5fb3c5956fe9dbf
destrike00 <k.aron147@gmail.com>
2023-05-06 18:45:00 +0200

    environment, interface for weather api usage

---
+++ weather-app/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city/landon',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'dc80d6ff74msh7dfc6144912889ap140d82jsn3ed9d082c74c',
+};
